84c39d4d5066dede9647ad18191d06fece8d0085
Thaina <thainalopes@gmail.com>
2024-01-09 22:18:33 -0300

    Ajustando texto do rodape

--- src/Rodape.js
+++ src/Rodape.js
@@ -3,16 +3,14 @@ import "./Rodape.css";
 export default function Rodape() {
   return (
     <footer className="rodape">
-      <p className="assinatura">
-        &copy; Desenvolvido por
-        <a
-          href="https://agencia-mive.vercel.app/"
-          target="_blank"
-          className="mive"
-        >
-          Mivê - Digital Marketing
-        </a>
-      </p>
+      <a
+        href="https://agencia-mive.vercel.app/"
+        target="_blank"
+        className="rodape-mive"
+      >
+        &copy;
+        <p className="assinatura"> mivê - Digital Marketing</p>
+      </a>
     </footer>
   );
 }
